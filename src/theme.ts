e358f0e080ccaf4206acc722a48afb0e92f28241
hayeon <hayun4475@gmail.com>
2022-05-12 23:59:12 +0900

    ts into

--- src/theme.ts
+++ src/theme.ts
@@ -4,7 +4,7 @@ export const lightTheme:DefaultTheme = {
     bgColor:"white",
     textColor:"black",
     btnColor:"tomato",
-};
+}; 
 
 export const darkTheme:DefaultTheme = {
     bgColor:"black",
