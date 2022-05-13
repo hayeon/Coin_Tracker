af8006531c0849e3a4923a1c437be1c1dc136c34
hayeon <hayun4475@gmail.com>
2022-05-14 05:02:06 +0900

    fetch

--- src/routes/CoinList.tsx
+++ src/routes/CoinList.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div `
     padding: 0px, 20px;
+    max-width : 480px;
+    margin : 0 auto;
 `;
 
 const Header = styled.header `
@@ -36,54 +40,58 @@ const Title = styled.h1 `
     color: ${(props) =>props.theme.accentColor};
 `;
 
+const Loader = styled.div`
+    text-align: center;
+    display: block;
+`;
+
+
+interface CoinInterface { //ts에게 알려주기 위한 interface
+        id: string,
+        name: string,
+        symbol: string,
+        rank: number,
+        is_new: boolean,
+        is_active: boolean,
+        type: string,
+        
+const Img = styled.img`
+width: 25px;
+height :25px;
+
+`};
+
 
 function CoinList () {
-
-    const coins = [
-        {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-        }
-
-    ];
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loding, setLoding] = useState(true);
+    useEffect(()=> {
+        (async() => {
+            const response = await fetch ("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0,100));
+            // setLoding(false);
+;
+            console.log(coins);
+        }) ();
+    }, []);
     return (
        <Container> 
            <Header>
-            <Title>코인 목록</Title>
+                <Title>코인 목록</Title>
            </Header>
-           <Coins>
+           {loding ? (<Loader>Loading</Loader>):(
+            <Coins>
                {coins. map((coin)=>(
                    <Coin key={coin.id}> 
                     <Link to={coin.id}>
+                        <img src= {`https://coinicons-api.vercel.app/${coin.symbol.toLocaleLowerCase()}`}></img>
                         {coin.name} &rarr;
                     </Link>
                    </Coin>
                ))}
              
-           </Coins>
+           </Coins>)}
         </Container>
     
     );
